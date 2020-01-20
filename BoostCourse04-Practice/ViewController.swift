a7a781b50a6f30ed31ed39494cac75fd982abd5f
joseph704 <freelove54@naver.com>
2020-01-20 20:42:57 +0900

    Initial Commit

---
+++ BoostCourse04-Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoostCourse04-Practice
+//
+//  Created by 차요셉 on 20/01/2020.
+//  Copyright © 2020 차요셉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
