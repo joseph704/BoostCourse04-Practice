19b594a4a3f179aeae0eed7b132c0919eae1553f
joseph704 <freelove54@naver.com>
2020-01-21 17:46:43 +0900

    Add edit methods which delete asset you want in ViewController.swift

--- BoostCourse04-Practice/ViewController.swift
+++ BoostCourse04-Practice/ViewController.swift
@@ -8,16 +8,16 @@
 
 import UIKit
 import Photos
-class ViewController: UIViewController,UITableViewDataSource {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate,PHPhotoLibraryChangeObserver {
     
-    
-
     @IBOutlet weak var tableView:UITableView!
+    
     var fetchResult: PHFetchResult<PHAsset>!
     let imageManager: PHCachingImageManager = PHCachingImageManager()
     let cellIdentifier: String = "cell"
     
     func requestCollection() {
+        
         let cameraRoll: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: . smartAlbum, subtype: . smartAlbumUserLibrary, options:nil)
         guard let cameraRollCollection = cameraRoll.firstObject else { return }
         
@@ -28,6 +28,7 @@ class ViewController: UIViewController,UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 사진첩 접근 권한
         let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
         
         switch photoAuthorizationStatus {
@@ -58,19 +59,41 @@ class ViewController: UIViewController,UITableViewDataSource {
             
         }
         
-        
+        PHPhotoLibrary.shared().register(self)
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fetchResult?.count ?? 0
+        return self.fetchResult?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
         
-        let asset: PHAsset = fetchResult.object(at: indexPath.row)
+        let asset: PHAsset = self.fetchResult.object(at: indexPath.row)
         imageManager.requestImage(for: asset, targetSize: CGSize(width: 30, height: 30), contentMode: .aspectFill, options: nil, resultHandler: {image, _ in cell.imageView?.image = image})
         return cell
+    }
+    
+    // 테이블뷰 편집 여부 메소드
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    // 편집모드로 들어 왔을 때 편집 스타일에 따른 메소드
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let asset: PHAsset = self.fetchResult[indexPath.row]
+            PHPhotoLibrary.shared().performChanges({PHAssetChangeRequest.deleteAssets([asset] as NSArray)}, completionHandler: nil)
+        }
+    }
+    
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        guard let changes = changeInstance.changeDetails(for: self.fetchResult) else { return }
+        
+        self.fetchResult = changes.fetchResultAfterChanges
+        
+        OperationQueue.main.addOperation {
+            self.tableView.reloadSections(IndexSet(0...0), with: .automatic)
+        }
     }
 
 }
